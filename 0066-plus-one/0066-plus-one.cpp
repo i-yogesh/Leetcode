9343e2fcc23e029fdf057900c7e2cd3488fed44d
i-yogesh <102923296+i-yogesh@users.noreply.github.com>
2022-10-14 21:42:11 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (12.50%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        
+        for(int i=digits.size()-1; i>=0; i--)
+        {
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            
+            digits[i]=0;
+        }
+        vector<int> ans(digits.size()+1);
+        ans[0]=1;
+        return ans;
+    }
+};
