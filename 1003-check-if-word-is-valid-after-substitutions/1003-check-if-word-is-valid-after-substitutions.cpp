3700fb368d476914c3fff20bb5741149fd67b803
Yogesh Mishra <102923296+i-yogesh@users.noreply.github.com>
2022-11-26 22:28:39 +0530

    Time: 926 ms (5.36%), Space: 8 MB (94.64%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        for(auto i=s.find("abc"); i!=string::npos; i=s.find("abc"))
+            s.erase(i,3);
+        if(s=="")
+            return true;
+        return false;
+    }
+};
