5d7b75de97b1097dd3c5da85a760fb3a00d41740
Yogesh Mishra <102923296+i-yogesh@users.noreply.github.com>
2023-03-28 21:12:41 +0530

    Added solution - LeetHub

---
+++ Activity Selection - GFG/activity-selection.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find the maximum number of activities that can
+    //be performed by a single person.
+    int activitySelection(vector<int> start, vector<int> end, int n)
+    {
+        vector<pair<int, int>> startEnd;
+        for(int i=0; i<n; i++){
+            startEnd.push_back({start[i], end[i]});
+        }
+        sort(startEnd.begin(), startEnd.end(), [&](auto a, auto b){
+           return a.second < b.second; 
+        });
+        int count=0,lastActivity=0;
+        for(auto i: startEnd){
+            if(i.first > lastActivity){
+                count++;
+                lastActivity = i.second;
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    
+    //testcases
+    cin >> t;
+    while(t--)
+    {
+        //size of array
+        int n;
+        cin >> n;
+        vector<int> start(n), end(n);
+        
+        //adding elements to arrays start and end
+        for(int i=0;i<n;i++)
+            cin>>start[i];
+        for(int i=0;i<n;i++)
+            cin>>end[i];
+        Solution ob;
+        //function call
+        cout << ob.activitySelection(start, end, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
